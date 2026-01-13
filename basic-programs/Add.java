bf418df003248cfcea1f2a35169c233d62d5ba85
Daksh <dkdakshkumar268@gmail.com>
2026-01-13 19:29:06 +0530

    Added a program to add two numbers

---
+++ basic-programs/Add.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Add{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers:");
+		double x=sc.nextDouble();
+		double y=sc.nextDouble();
+		System.out.println("Sum of the given numbers= "+(x+y));
+	}
+}
+		
